1e032e7de93c5dd3a473bd380a06d45122bb9517
Dongcheng Gai <19871289@qq.com>
2016-01-17 02:39:21 -0800

    Init Upload

---
+++ app/src/main/java/cn/gdc/javacoding/Utilities/MyTextUtil.java
@@ -0,0 +1,10 @@
+package cn.gdc.javacoding.Utilities;
+
+/**
+ * Created by Administrator on 2016/1/16.
+ */
+public class MyTextUtil {
+    public String getValue() {
+        return "Hello from the utility class";
+    }
+}
