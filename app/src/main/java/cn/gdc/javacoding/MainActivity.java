cbf46ca6b461108011a4f514e7a0826fa6165722
Dongcheng Gai <19871289@qq.com>
2016-01-19 16:28:12 -0800

    Added log info on 2016/1/19 by Gai Dongcheng

--- app/src/main/java/cn/gdc/javacoding/MainActivity.java
+++ app/src/main/java/cn/gdc/javacoding/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWelcomeString();
+
+        for (int count = 0; count < 10; count++) {
+            Log.i("MainActivity", "count = " + count);
+
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
